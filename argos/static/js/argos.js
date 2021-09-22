b305157564cfc4913c2b7778031a2a9a704a1047
danilo.souza <danilo.souza@daimon.com.br>
2021-09-22 16:21:41 -0300

    Inclusão de função para salvar a última aba selecionada pelo usuário.

--- argos/static/js/argos.js
+++ argos/static/js/argos.js
@@ -97,6 +97,27 @@ function getSetScrollTop() {
 }
 
 /*
+Função para salvar a última aba selecionada pelo usuário. Ela é recuperada após
+qualquer refresh da página.
+*/
+function getSetActiveTab(){
+    $(function() {
+        // for bootstrap 3 use 'shown.bs.tab', for bootstrap 2 use 'shown' in the next line
+        $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+            // save the latest tab
+            localStorage.setItem('lastTab', $(this).attr('href'));
+        });
+
+        // go to the latest tab, if it exists:
+        var lastTab = localStorage.getItem('lastTab');
+        if (lastTab) {
+            $('[href="' + lastTab + '"]').tab('show');
+        }
+    });
+}
+
+
+/*
 Função para adicionar um evento a cada objeto da classe
 dropdown-toggle para impedir que um deles colapse quando outro
 for expandido. A função lê todos os elementos da classe dropdown-toggle.
